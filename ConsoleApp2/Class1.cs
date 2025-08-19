5cbe6d4db13f901a058d433328ac62a408eb562b
Nick W <nickwilliamson017@gmail.com>
2025-08-19 10:57:10 +0800

    added .net server

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal static class Class1
+    {
+        static void Main(String[] args)
+        {
+            Console.Title = "Bank Data Server";
+            Console.WriteLine("Starting server...");
+
+            var tcp = new NetTcpBinding();
+
+            using (ServiceHost host = new ServiceHost(typeof(DataServer)))
+            {
+                host.AddServiceEndpoint(typeof(DataServerInterface), tcp,
+                    "net.tcp://0.0.0.0:8100/DataService");
+
+                host.Open();
+                Console.WriteLine("System Online at net.tcp://0.0.0.0:8100/DataService");
+                Console.WriteLine("Press ENTER to stop.");
+                Console.ReadLine();
+                host.Close();
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/DataServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ClassLibrary1;
+
+namespace ConsoleApp2
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, UseSynchronizationContext = false)]
+    internal class DataServer : DataServerInterface
+    {
+        private readonly DatabaseClass db = new DatabaseClass();
+        public int GetNumEntries() => db.getNumRecords();
+        public void GetValuesForEntry(int index, out uint acctNo, out uint pin, out int bal, out string firstName, out string lastName)
+        {
+            if (index < 0 || index >= db.getNumRecords())
+                throw new FaultException($"Index  {index} is out of range.");
+
+            acctNo = db.GetAcctNoByIndex(index);
+            pin = db.GetPINByIndex(index);
+            bal = db.GetBalanceByIndex(index);
+            firstName = db.GetFirstNameByIndex(index);
+            lastName = db.GetLastNameByIndex(index);
+        }
+    }
+}
